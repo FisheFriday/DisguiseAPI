a00964564174eed23400d88ff446689f42030701
FisheFriday <fishefriday@gmail.com>
2025-08-07 11:32:34 -0400

    Delete API directory (for the new one)

--- API/src/main/java/net/pinger/disguise/player/info/PlayerNameUpdateAction.java
+++
@@ -1,7 +0,0 @@
-package net.pinger.disguise.player.info;
-
-public class PlayerNameUpdateAction extends PlayerUpdateAction<String> {
-    protected PlayerNameUpdateAction(String name) {
-        super(PlayerUpdateInfoAction.NICK, name);
-    }
-}
